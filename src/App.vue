2ddc678c2df175ac28172ffed17c6811353d57a9
Stareven233 <1429960342@qq.com>
2020-10-09 23:22:29 +0800

    feat: Use element to implement a basic demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-input
+      class="input-area"
+      type="textarea"
+      :autosize="{ minRows: 4, maxRows: 10}"
+      resize="none"
+      clearable
+      placeholder="请输入内容"
+      v-model="textarea1">
+    </el-input>
+     <el-button @click="generateTree">生成</el-button>
+
+    <el-tree class="tree"  :data="treeData" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    <!-- 添加属性default-expand-all 默认展开所有节点 -->
+
+    <span>{{testText}}</span>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  // ↓使用return包裹后数据中变量只在当前组件中生效，不会影响其他组件，防止数据污染
+  data () {
+    return {
+      textarea1: '',
+      testText: 'testText',
+
+      oneData: {
+        label: '9级 1',
+        children: [{
+          label: '9级 1-1'
+        }]
+      },
+
+      treeData: [{
+        label: '一级 1',
+        children: [{
+          label: '二级 1-1',
+          children: [{
+            label: '三级 1-1-1'
+          }]
+        }]
+      }, {
+        label: '一级 2',
+        children: [{
+          label: '二级 2-1',
+          children: [{
+            label: '三级 2-1-1'
+          }]
+        }, {
+          label: '二级 2-2',
+          children: [{
+            label: '三级 2-2-1'
+          }]
+        }]
+      }, {
+        label: '一级 3',
+        children: [{
+          label: '二级 3-1',
+          children: [{
+            label: '三级 3-1-1'
+          }]
+        }, {
+          label: '二级 3-2',
+          children: [{
+            label: '三级 3-2-1'
+          }]
+        }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+        // 定义上述要生成节点的对象列表中所采用的属性名
+      }
+    }
+  },
+  methods: {
+    handleNodeClick (data) {
+      console.log(data)
+    },
+    generateTree (e) {
+      if (!this.textarea1) {
+        return false
+      }
+      this.testText = this.textarea1
+      this.textarea1 = ''
+      this.treeData.push(this.oneData)
+    }
   }
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto 0;
+  width: 800px;
+
+  .tree {
+    margin: 20px 0;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+// $mount('#app') ：手动挂载到id为app的dom中
+// 当Vue实例没有el属性时，可以之后调用vm.$mount()方法来延迟挂载
